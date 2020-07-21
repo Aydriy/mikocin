8620318d44f5575987a1691b0d851afc9849a5d7
Aydriy <smitas4511@gmail.com>
2020-07-21 21:29:00 +0300

    Updates

---
+++ precache-manifest.a33f95b86c8e6cf720778bead6d1a6cb.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab6ec62490b1b853af1a9e714f696ef",
+    "url": "/mikocin/index.html"
+  },
+  {
+    "revision": "7987f4e7a6bda6f2b81a",
+    "url": "/mikocin/static/css/main.1af8618e.chunk.css"
+  },
+  {
+    "revision": "2f209478605014e8bede",
+    "url": "/mikocin/static/js/2.d9777b54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mikocin/static/js/2.d9777b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7987f4e7a6bda6f2b81a",
+    "url": "/mikocin/static/js/main.8aebf80e.chunk.js"
+  },
+  {
+    "revision": "67191d373037e06fb7fd",
+    "url": "/mikocin/static/js/runtime-main.7b643242.js"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/mikocin/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/mikocin/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "a720f17aa773e493a7ebf8b08459e66c",
+    "url": "/mikocin/static/media/Roboto-Italic.a720f17a.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/mikocin/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/mikocin/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/mikocin/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "643ca93cb101f14caf10459d9048e878",
+    "url": "/mikocin/static/media/circleInner.643ca93c.png"
+  },
+  {
+    "revision": "b79c2bd597a8fc37615c71d4018deb80",
+    "url": "/mikocin/static/media/circleOuter.b79c2bd5.png"
+  },
+  {
+    "revision": "e8139451811e4512648f2c859c3aaed4",
+    "url": "/mikocin/static/media/form1.e8139451.png"
+  },
+  {
+    "revision": "d3d5bf32f4ae3776e194005eab0d04e5",
+    "url": "/mikocin/static/media/form2.d3d5bf32.png"
+  },
+  {
+    "revision": "0e14da5c8f3fcc32f379cf6605829ee0",
+    "url": "/mikocin/static/media/form3.0e14da5c.png"
+  },
+  {
+    "revision": "1b74c81bcaf8942658f23e35f40e095d",
+    "url": "/mikocin/static/media/green-leavs.1b74c81b.png"
+  },
+  {
+    "revision": "3ff38c35d5711969664e731361252344",
+    "url": "/mikocin/static/media/header-bg.3ff38c35.png"
+  },
+  {
+    "revision": "7a6d197c168135aa5d0c5a93911b5e59",
+    "url": "/mikocin/static/media/header-mask.7a6d197c.png"
+  },
+  {
+    "revision": "8e7788cf2670a2a559bbf2c9f78ed781",
+    "url": "/mikocin/static/media/leg.8e7788cf.png"
+  },
+  {
+    "revision": "bba11e9e9f342f0bb945e6b655fd8f6a",
+    "url": "/mikocin/static/media/pills-white.bba11e9e.png"
+  },
+  {
+    "revision": "743454d20f19099027b987d46eab37d1",
+    "url": "/mikocin/static/media/pills1.743454d2.png"
+  },
+  {
+    "revision": "f50c9ecd09bbfe4441efedf822f923fa",
+    "url": "/mikocin/static/media/pills2.f50c9ecd.png"
+  },
+  {
+    "revision": "1e49af2a4d3efc03864878a23c2efb83",
+    "url": "/mikocin/static/media/pills3.1e49af2a.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mikocin/precache-manifest.a33f95b86c8e6cf720778bead6d1a6cb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mikocin/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
